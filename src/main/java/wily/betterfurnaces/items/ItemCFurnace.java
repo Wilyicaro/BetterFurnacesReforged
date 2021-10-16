54d46660196f2c5044867752cb3ad22ecd839281
Wilyicaro <icarokauanbonfim@hotmail.com>
2021-10-16 19:50:18 -0300

    First 1.12 Update

---
+++ src/main/java/wily/betterfurnaces/items/ItemCFurnace.java
@@ -0,0 +1,13 @@
+package wily.betterfurnaces.items;
+
+import wily.betterfurnaces.BetterFurnacesReforged;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+
+public class ItemCFurnace extends Item {
+    public ItemCFurnace(String name) {
+        this.setTranslationKey(BetterFurnacesReforged.MODID + "." + name);
+        this.setRegistryName(new ResourceLocation(BetterFurnacesReforged.MODID, name));
+        this.setMaxStackSize(1);
+    }
+}
