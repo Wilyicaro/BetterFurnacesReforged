4bd99577b4f55bca38b6991319656711db46d3e9
Wilyicaro <icarokauanbonfim@hotmail.com>
2021-11-07 03:37:37 -0300

    -Added Update Checker of Iron Furnaces Mod -Extreme Forge's cull face corrected

--- src/main/java/wily/betterfurnaces/BetterFurnacesReforged.java
+++ src/main/java/wily/betterfurnaces/BetterFurnacesReforged.java
@@ -24,7 +24,7 @@ public class BetterFurnacesReforged
 {
 
     public static final String MOD_ID = "betterfurnacesreforged";
-    public static final String VERSION = "11";
+    public static final String VERSION = "12";
     public static final String MC_VERSION = "1.17.1";
 
     public static final Logger LOGGER = LogManager.getLogger();
