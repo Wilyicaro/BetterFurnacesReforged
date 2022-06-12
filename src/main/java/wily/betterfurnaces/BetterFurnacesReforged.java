99f6a60de6a638300ed6a88ffdfb12f06bc77310
Wilyicaro <icarokauanbonfim@hotmail.com>
2022-06-12 09:19:08 -0300

    Release 1.0 -Fixed Github update checker version

--- src/main/java/wily/betterfurnaces/BetterFurnacesReforged.java
+++ src/main/java/wily/betterfurnaces/BetterFurnacesReforged.java
@@ -31,7 +31,7 @@ public class BetterFurnacesReforged
 
     public static final String MOD_ID = "betterfurnacesreforged";
     public static final String VERSION = "100";
-    public static final String MC_VERSION = "1.18.2";
+    public static final String MC_VERSION = "1.19";
 
     public static final Logger LOGGER = LogManager.getLogger();
 
