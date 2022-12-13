1a61fafb19b010263d0376e4b9d86bb415f529c1
Wilyicaro <icarokauanbonfim@hotmail.com>
2022-12-13 17:06:38 -0300

    Release 1.3.2 Features Ported from 1.19.2 mod version -Added compatibility with Project MMO 2.0 -Fixed Iron Upgrade dupe bug -Optimized Factory type upgrades -Added drop with furnace data when block is broken with a silk touch enchanted pickaxe -Now furnaces and forges support color tint when is an item -Fixed Xp tank Upgrade crash and not working with any optional dependencies fluid

--- src/main/java/wily/betterfurnaces/BetterFurnacesReforged.java
+++ src/main/java/wily/betterfurnaces/BetterFurnacesReforged.java
@@ -31,7 +31,7 @@ public class BetterFurnacesReforged
 {
 
     public static final String MOD_ID = "betterfurnacesreforged";
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.2";
     public static final String MC_VERSION = "1.18.2";
 
     public static final Logger LOGGER = LogManager.getLogger();
