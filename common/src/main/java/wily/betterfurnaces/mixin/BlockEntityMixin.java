043be189fe3700d75729afadf97f9ad9e0cd54d8
Wilyicaro <icarokauanbonfim@hotmail.com>
2023-11-18 12:09:48 -0300

    Release 1.16.5-1.1.3 -Fixed crash in dedicated server, because of the non-existence of method Registry.containsKey in server side

--- common/src/main/java/wily/betterfurnaces/mixin/BlockEntityMixin.java
+++ common/src/main/java/wily/betterfurnaces/mixin/BlockEntityMixin.java
@@ -6,10 +6,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -20,13 +17,11 @@ import java.util.Optional;
 @Mixin(BlockEntity.class)
 public class BlockEntityMixin {
 
-    @Shadow @Final private static Logger LOGGER;
-
     @Inject(method = "loadStatic", at = @At("HEAD"), cancellable = true)
     private static void loadStatic(BlockState blockState, CompoundTag compoundTag, CallbackInfoReturnable<BlockEntity> cir){
         ResourceLocation id = new ResourceLocation(compoundTag.getString("id"));
         Optional<BlockEntityType<?>> opt = Registry.BLOCK_ENTITY_TYPE.getOptional(id);
-        if ((blockState.getBlock() instanceof SmeltingBlock || Registry.BLOCK.containsKey(id)) && !opt.isPresent()) {
+        if (blockState.getBlock() instanceof SmeltingBlock && !opt.isPresent()) {
             BlockEntity entity = ((SmeltingBlock)Registry.BLOCK.get(id)).newBlockEntity(null);
             if (entity!= null) entity.load(blockState,compoundTag);
             cir.setReturnValue(entity);
